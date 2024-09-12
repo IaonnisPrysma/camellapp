bd937f420f980898224420b93d9f4dbcd70b3a03
Juan Diego Jimenez Bedoya <juandiego38@hotmail.es>
2024-09-12 13:40:52 -0500

    Huge changes
    Like, several, really, don't bother for now

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,20 @@
-console.log('¡Dios mío que pereza!');
+document.addEventListener('DOMContentLoaded', function () {
+    const tabs = document.querySelectorAll('.tab');
+    const tabContents = document.querySelectorAll('.tab-content');
+
+    tabs.forEach(tab => {
+        tab.addEventListener('click', () => {
+            const tabName = tab.getAttribute('data-tab');
+
+            tabContents.forEach(content => {
+                content.classList.remove('active');
+            });
+            document.getElementById(tabName).classList.add('active');
+
+            tabs.forEach(t => {
+                t.classList.remove('active');
+            });
+            tab.classList.add('active');
+        });
+    });
+});
